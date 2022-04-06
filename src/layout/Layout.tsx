ad381cb38a538d1314e4fa245344235c1352a371
Stalkakuma <stalkakuma@gmail.com>
2022-04-06 19:08:15 +0300

    layout

---
+++ src/layout/Layout.tsx
@@ -0,0 +1,10 @@
+import './layout.scss';
+
+export const Layout = () => {
+  return (
+    <div className='layout'>
+      <aside className='layout__navigation'></aside>
+      <main className='layout__content'></main>
+    </div>
+  );
+};
